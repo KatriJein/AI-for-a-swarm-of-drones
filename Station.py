42c1de67070aaeb237107f35e54c5abe1ee043cc
DariaRozanova <131370089+DariaRozanova@users.noreply.github.com>
2023-12-02 15:29:48 +0500

    Add files via upload
    Добавлены отдельные места со своими координатами и вспомогательные функции. Изменена функция отрисовки

--- Station.py
+++ Station.py
@@ -1,3 +1,4 @@
+
 import turtle
 
 WIDTH, HEIGHT = 1000, 600
@@ -12,29 +13,27 @@ koef = 10
 class Station:
     def __init__(self):
         self._energy = 10000
-        self._width = 4 * koef
-        self._height = 8 * koef
-        self._x = WIDTH / 2
-        self._y = HEIGHT / 2 + (self._height / 2)
-        self._places = {}
-        for i in range(40):
-            self._places[i] = None
+        self._width = 8 * koef
+        self._height = 4 * koef
+        self._x = WIDTH / 2 - self._width
+        self._y = HEIGHT / 2 - self._height / 2
+        self._places_coordinates = {}
+        self._places_drones = {}
+        self._places_count = 32
+        self.places_width = self._places_count // 4
+        self.places_height = self._places_count // self.places_width
+        self._place_size = self._width // 4
 
-    def draw(self):
-        t = turtle.Turtle()
-        t.hideturtle()
-        t.penup()
-        t.speed(0)
-        t.setposition(self._x, self._y)
-        t.color("red")
-        t.pendown()
-        t.begin_fill()
-        for i in range(2):
-            t.forward(self._width)
-            t.right(90)
-            t.forward(self._height)
-            t.right(90)
-        t.end_fill()
+        arr = []
+        for i in range(self.places_width):
+            for j in range(self.places_height):
+                arr.append((i * self._place_size + self._x, j * self._place_size + self._y))
+
+        for i in range(self._places_count):
+            self._places_coordinates[i] = arr[i]
+
+        for i in range(self._places_count):
+            self._places_drones[i] = None
 
     def charge(self):
         '''Заряжает станцию, если ее заряд ниже 5 тысяч'''
@@ -50,26 +49,57 @@ class Station:
         '''Возвращает координаты станции'''
         return self._x, self._y
 
-    def get_places(self):
+    def get_places_coordinates(self):
+        '''Возврщает словарь, ключ - номер места, значение - левые нижние координаты места'''
+        return self._places_coordinates
+
+    def get_place_coordinates(self, place_id):
+        '''Возвращает координаты определенного места'''
+        return self._places_coordinates[place_id]
+
+    def get_drone_place_coordinates(self, drone_id):
+        '''Возвращает координаты места дрона по индексу последнего'''
+        return self._places_coordinates[list(self._places_drones.values()).index(drone_id)]
+
+    def draw(self):
+        '''Отрисовка станции'''
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        t.speed(0)
+        t.color('black')
+        t.setposition(self._x, self._y)
+        t.fillcolor('yellow')
+        for i in range(self._places_count):
+            t.setposition(self._places_coordinates[i])
+            t.pendown()
+            t.begin_fill()
+            for j in range(4):
+                t.forward(self._place_size)
+                t.left(90)
+            t.end_fill()
+            t.penup()
+
+    def get_places_drones(self):
         '''Вовращает места станции ввиде словаря, где ключ - номер места, значение - id дрона'''
-        return self._places
+        return self._places_drones
 
     def count_free_places(self):
         '''Вовращает количество свободных мест'''
         count = 0
-        for key in self._places.keys():
-            if not self._places[key]:
+        for key in self._places_drones.keys():
+            if not self._places_drones[key]:
                 count += 1
         return count
-    
+
     def get_free_places_ids(self):
         '''Возвращает номера свободных мест'''
         ids = []
-        for k in self._places.keys():
-            if not self._places[k]:
+        for k in self._places_drones.keys():
+            if not self._places_drones[k]:
                 ids.append(k)
         return ids
-    
+
     def charge_drone(self, drone):
         '''Пополняет заряд дрона за счет заряда станции'''
         while (not drone.is_full_energy()) and self._energy > 0:
@@ -79,18 +109,17 @@ class Station:
     def set_drone(self, drone):
         '''Присваивает дрону свободное место, если свободных мест нет, то переводит дрон в режим ожидания'''
         if self.count_free_places() > 0:
-            keys = list(self._places.keys())
-            index = list(self._places.values()).index(None)
-            self._places[keys[index]] = drone.get_id()
+            keys = list(self._places_drones.keys())
+            index = list(self._places_drones.values()).index(None)
+            self._places_drones[keys[index]] = drone.get_id()
         else:
             drone.wait()
 
     def remove_drone(self, drone):
         '''Убирает дрон со своего места'''
-        for k in self._places.keys():
-            if self._places[k] == drone.get_id():
-                self._places[k] = None
-    
+        for k in self._places_drones.keys():
+            if self._places_drones[k] == drone.get_id():
+                self._places_drones[k] = None
 
 st = Station()
 st.draw()
